0aa12067474c2a0db4b362e29f4d915e520e39df
Amaurys Almanzar <amaurys.almanzar@wework.com>
2019-07-15 01:00:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -22,6 +22,10 @@ function whileLoop (n) {
 
 function doWhileLoop(num){
 
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
   do {
     console.log("I run once regardless.");
   } while (incrementVariable() < num);
